06317449add9f457f2b16229c44b29977b13b9e1
Zkwai <49253199+Zkwai@users.noreply.github.com>
2025-12-27 16:35:59 +0100

    Create pitch.tsx

---
+++ src/pages/pitch.tsx
@@ -0,0 +1,13 @@
+import Navigation from "@/components/Navigation";
+import AvisCertifiesPitch from "@/components/AvisCertifiesPitch";
+
+const PitchPage = () => {
+  return (
+    <div>
+      <Navigation />
+      <AvisCertifiesPitch />
+    </div>
+  );
+};
+
+export default PitchPage;
